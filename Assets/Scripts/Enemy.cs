08ae202d73f3f18f380cc736c011302f499b59a0
Jaimy Koster <43574560+RekCus@users.noreply.github.com>
2019-05-24 22:30:41 +0200

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public int health;
+
+   // public GameObject deathEffect;
+    public void TakeDamage (int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+      //Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Melee")
+        {
+            TakeDamage(100);
+        }
+    }
+}

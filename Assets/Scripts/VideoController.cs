fe59f206a689b171b398ba1dab28fec65aa7fd35
Jaimy Koster <43574560+RekCus@users.noreply.github.com>
2019-06-25 00:10:49 +0200

    Cutscenes added
    and new music

---
+++ Assets/Scripts/VideoController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoController : MonoBehaviour
+{
+    public GameObject ui;
+    public VideoPlayer vid;
+    //vid.loopPointReached += EndReached;
+    public string sceneToLoad;
+
+
+
+   private void Start()
+    {
+        vid.loopPointReached += EndReached;
+        ui.SetActive(false);
+    }
+
+
+    public void loadNextScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+        {
+            Debug.Log("Video End");
+            ui.SetActive(true);
+            loadNextScene();
+        }
+    
+}
